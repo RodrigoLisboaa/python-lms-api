f28c53ba04f3bbf8230f41ddaa6666386050259c
RodrigoLisboaa <rodrigo.s.lisboa7@gmail.com>
2026-02-28 10:54:58 -0300

    chore(config): adiciona variaveis de auth via env

--- app/core/config.py
+++ app/core/config.py
@@ -7,5 +7,8 @@ class Settings(BaseSettings):
     ENV: str = "local"
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
 settings = Settings()
